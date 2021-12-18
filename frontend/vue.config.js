5bcb897653c691b02723b2e341d2044e468b14dd
herschel-ma <maliang_gs@126.com>
2021-12-18 09:13:00 +0800

    更改打包路径

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 var BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
-  // publicPath: "http://127.0.0.1:8000/",
-  publicPath: "/static/",
+  publicPath:
+    process.env.NODE_ENV !== "production" ? "http://127.0.0.1:8080/" : "./",
   outputDir: "./dist/",
   runtimeCompiler: true,
 
