80cef2b30c4a573e4ea9b63d852f5f2794d412f4
herschel-ma <maliang_gs@126.com>
2021-12-30 19:40:27 +0800

    修改asgi http规则

--- BlogApp/asgi.py
+++ BlogApp/asgi.py
@@ -13,10 +13,14 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogApp.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    "http":
+    django_asgi_app,
     "websocket":
     AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
 })
