389dcaebe8a64f7a04763ba909870d3b5d2b94b4
herschel-ma <maliang_gs@126.com>
2022-01-01 20:01:00 +0800

    fix：生产环境部署失败->更改asgi导包顺序

--- BlogApp/asgi.py
+++ BlogApp/asgi.py
@@ -12,11 +12,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogApp.settings')
 django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http":
